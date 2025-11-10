c395cb0d9e23083cb06cfa47990a74ac7a28b0f8
yxia216 <yxia216@bloomberg.net>
2025-11-10 14:08:48 -0500

    fix-tests
    Signed-off-by: yxia216 <yxia216@bloomberg.net>

--- internal/extproc/translator/openai_gcpvertexai.go
+++ internal/extproc/translator/openai_gcpvertexai.go
@@ -371,11 +371,6 @@ func (o *openAIToGCPVertexAITranslatorV1ChatCompletion) openAIMessageToGeminiMes
 	if err != nil {
 		return nil, fmt.Errorf("error converting tool choice: %w", err)
 	}
-	if openAIReq.EnterpriseWebSearch {
-		tools = append(tools, genai.Tool{
-			EnterpriseWebSearch: &genai.EnterpriseWebSearch{},
-		})
-	}
 
 	// Convert generation config.
 	generationConfig, responseMode, err := openAIReqToGeminiGenerationConfig(openAIReq, requestModel)
@@ -420,6 +415,12 @@ func (o *openAIToGCPVertexAITranslatorV1ChatCompletion) applyVendorSpecificField
 	}
 	if gcpVendorFields.SafetySettings != nil {
 		gcr.SafetySettings = gcpVendorFields.SafetySettings
+	}
+
+	if gcpVendorFields.EnterpriseWebSearch {
+		gcr.Tools = append(gcr.Tools, genai.Tool{
+			EnterpriseWebSearch: &genai.EnterpriseWebSearch{},
+		})
 	}
 }
 
